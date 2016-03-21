a57a476d504a4662ce121727a1df1b2b6cc40957
Alexlion2308 <alexandreleconquerant@gmail.com>
2016-03-21 16:18:48 +0100

    Facade/Factory/Gestionnaire commit

---
+++ src/Factory.java
@@ -0,0 +1,148 @@
+import Persistance.*;
+import TabPatterns.*;
+import UIPackage.*;
+import BusinessLogic.*;
+import MainPackage.*;
+
+
+public class Factory {
+
+	
+
+	public Factory() {
+	}
+
+	public JDBCPerson createJDBCPerson (String nicknameIn, String passwordIn) {
+		// futur : phase de test pour voir si le nickname ou le password est pertinant
+		return new JDBCPerson(nicknameIn, passwordIn);
+	}
+	
+	public JDBCUser createJDBCUser () {
+		return new JDBCUser();
+	}
+	
+	public JDBCAdmin createJDBCAdmin () {
+		return new JDBCAdmin();
+	}
+
+	public JDBCSeller createJDBCSeller () {
+		return new JDBCSeller();
+	}
+
+	public JDBCProduct createJDBCProduct () {
+		return new JDBCProduct();
+	}
+
+	public JDBCProductCategory createJDBCProductCategory () {
+		return new JDBCProductCategory();
+	}
+
+	public JDBCItem createJDBCItem () {
+		return new JDBCItem();
+	}
+
+	public JDBCCommand createJDBCCommand () {
+		return new JDBCCommand();
+	}
+
+	public JDBCBasket createJDBCBasket () {
+		return new JDBCBasket();
+	}
+
+	public JDBCPersonalProject createJDBCPersonalProject () {
+		return new JDBCPersonalProject();
+	}
+
+	public JDBCPersonalTask createJDBCPersonalTask () {
+		return new JDBCPersonalTask();
+	}
+
+	public JDBCGeneralProject createJDBCGeneralProject () {
+		return new JDBCGeneralProject();
+	}
+
+	public JDBCGeneralTask createJDBCGeneralTask () {
+		return new JDBCGeneralTask();
+	}
+
+	public JDBCProjectCategory createJDBCProjectCategory () {
+		return new JDBCProjectCategory();
+	}
+
+	public JDBCTaskCategory createJDBCTaskCategory () {
+		return new JDBCTaskCategory();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+
+	public JDBC createJDBC () {
+		return new JDBC();
+	}
+	
+}
---
+++ src/Gestionnaire.java
@@ -0,0 +1,9 @@
+
+public class Gestionnaire {
+
+	public Gestionnaire() {
+	}
+	
+	
+
+}
